dd5ae8afb95b781e869fd5a74d5c864eb1c353e3
toando249 <toando249@gmail.com>
2022-02-13 13:09:50 +0700

    Update FacebookAPI.ts

--- src/interfaces/FacebookAPI.ts
+++ src/interfaces/FacebookAPI.ts
@@ -10,11 +10,7 @@ export interface SendAttachmentPayload {
   is_reusable?: boolean;
   template_type?: string;
   text?: string;
-  buttons?: {
-    type: string;
-    title: string;
-    url: string;
-  };
+  buttons?: Array<SendButton>;
 }
 
 export interface SendAttachment {
