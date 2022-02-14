6194fe0ae2382fa91586d85029ce27da83df01e8
toando249 <toando249@gmail.com>
2022-02-14 14:10:40 +0700

    Update chatible.ts

--- src/services/chatible.ts
+++ src/services/chatible.ts
@@ -158,7 +158,8 @@ const forwardMessage = async (sender: string, receiver: string, data: WebhookMes
           method: 'get',
           parse: 'json',
         });
-        await fb.sendTextButtons(receiver, JSON.stringify(res.body), false, true, false, false, true);
+        urlsh = console.log(res.body);
+        await fb.sendTextButtons(receiver, urlsh, false, true, false, false, true);
       } else if (type === 'audio' || type === 'file') {
         await fb.sendAttachment(sender, receiver, type, data.attachments[0].payload.url, false, false, true);
       } else {
@@ -175,7 +176,8 @@ const forwardMessage = async (sender: string, receiver: string, data: WebhookMes
           method: 'get',
           parse: 'json',
         });
-      await fb.sendTextButtons(receiver, JSON.stringify(res.body), false, true, false, false, true);
+        urlsh = console.log(res.body);
+      await fb.sendTextButtons(receiver, urlsh, false, true, false, false, true);
       } else if (type === 'audio' || type === 'file') {
         await fb.sendAttachment(sender, receiver, type, data.attachments[0].payload.url, false, false, true);
       }
