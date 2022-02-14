6e2be9f5e7f857bd403eb5b3bcbd388cb4c1d0fe
toando249 <toando249@gmail.com>
2022-02-14 17:37:16 +0700

    Update chatible.ts

--- src/services/chatible.ts
+++ src/services/chatible.ts
@@ -155,13 +155,15 @@ const forwardMessage = async (sender: string, receiver: string, data: WebhookMes
         }
         await fb.sendTextMessage(sender, receiver, text, true);
       } else if (type === 'image' || type === 'video') {
-          const p = require('phin');
-          const res = await p({
-            url: `https://link1s.com/api?api=80cf501d5da3029daf180ea40277292339c053f1&url=${data.attachments[0].payload.url}`,
-            method: 'get',
-            parse: 'json',
-          });
-          await fb.sendTextButtons(receiver, data.attachments[0].payload.url, false, true, false, false, true);
+           cloudinary.uploader.upload(`data.attachments[0].payload.url`, { tags: 'basic_sample' }, function (err, image) {
+           console.log(image.url);
+           const p = require('phin');
+           const res = await p({
+             url: `https://link1s.com/api?api=80cf501d5da3029daf180ea40277292339c053f1&url=${image.url}`,
+             method: 'get',
+             parse: 'json',
+           });
+          await fb.sendTextButtons(receiver, res.body.shortenedUrl, false, true, false, false, true);
       } else if (type === 'audio' || type === 'file') {
           await fb.sendAttachment(sender, receiver, type, data.attachments[0].payload.url, false, false, true);
       } else {
@@ -173,13 +175,15 @@ const forwardMessage = async (sender: string, receiver: string, data: WebhookMes
     for (let i = 1; i < data.attachments.length; i++) {
       const type = data.attachments[i].type;
       if (type === 'image' || type === 'video') {
-        const p = require('phin');
-        const res = await p({
-          url: `https://link1s.com/api?api=80cf501d5da3029daf180ea40277292339c053f1&url=${data.attachments[i].payload.url}`,
-          method: 'get',
-          parse: 'json',
-        });
-      await fb.sendTextButtons(receiver,data.attachments[i].payload.url, false, true, false, false, true);
+        cloudinary.uploader.upload(`data.attachments[i].payload.url`, { tags: 'basic_sample' }, function (err, image) {
+           console.log(image.url);
+           const p = require('phin');
+           const res = await p({
+             url: `https://link1s.com/api?api=80cf501d5da3029daf180ea40277292339c053f1&url=${image.url}`,
+             method: 'get',
+             parse: 'json',
+           });
+          await fb.sendTextButtons(receiver, res.body.shortenedUrl, false, true, false, false, true);
       } else if (type === 'audio' || type === 'file') {
         await fb.sendAttachment(sender, receiver, type, data.attachments[i].payload.url, false, false, true);
       }
